5a9bff2fe8d624fc5a16bf313c9784f9e0cff832
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-04 00:02:46 +0700

    feat: create list, details, form component for app-page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
 
 const routes: Routes = [
   { path: 'movies', component: MainComponent }
 ]
 
+const COMPONENTS = [
+  MainComponent,
+  MovieListComponent,
+  MovieDetailsComponent,
+  MovieFormComponent
+]
+
 @NgModule({
   declarations: [
-    MainComponent
+    ...COMPONENTS
   ],
   imports: [
     CommonModule,
