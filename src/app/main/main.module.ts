faa1d606eb185c38d17111fb8868051f7d5ea785
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-06 10:58:10 +0700

    chore: install tailwind, fontawesome

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MovieFormComponent } from './movie-form/movie-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const routes: Routes = [
   { path: 'movies', component: MainComponent }
@@ -23,6 +24,7 @@ const COMPONENTS = [
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     RouterModule.forChild(routes)
   ],
   exports: [
