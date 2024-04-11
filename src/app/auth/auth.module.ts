7d4e118c3b57469df711e5e6b0f0159839c88950
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-11 23:26:48 +0700

    chore: install primeng + init auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './_components/sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent }
+  { path: 'auth', redirectTo: 'auth/sign-in' },
+  { path: 'auth/sign-in', component: SignInComponent }
 ]
 
 @NgModule({
   declarations: [
     AuthComponent,
+    SignInComponent
   ],
   imports: [
     CommonModule,
