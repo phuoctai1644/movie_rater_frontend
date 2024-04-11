9e128628d2d3307988dd6f979eee7a803e76547f
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-12 00:28:45 +0700

    feat: static html for sign-in page

--- src/app/auth/_components/sign-in/sign-in.component.ts
+++ src/app/auth/_components/sign-in/sign-in.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: [, [Validators.required]],
+      password: [, [Validators.required]]
+    })
+  }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './_components/sign-in/sign-in.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'auth', redirectTo: 'auth/sign-in' },
@@ -16,6 +19,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
     RouterModule.forChild(routes)
   ],
   exports: [
