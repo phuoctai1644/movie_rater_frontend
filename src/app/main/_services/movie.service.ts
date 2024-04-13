d548a5f685c900f8e71e2d68498d11a6e35583f3
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-13 15:02:50 +0700

    fix: get token from cookie

--- src/app/main/_services/movie.service.ts
+++ src/app/main/_services/movie.service.ts
@@ -2,16 +2,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ApiResponse, Movie, Rating, RatingPayload } from '../_models';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  token = this.cookieService.get('accessToken');
   private headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Authorization': `Token a5d9b0dcb4fa751eb5f7ea17cfa4fcd4a339b697`,
+    'Authorization': `Token ${this.token}`,
   })
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) { }
 
   getAll() {
     return this.http.get<Movie[]>(`${environment.apiUrl}/movies/`, { headers: this.headers});
