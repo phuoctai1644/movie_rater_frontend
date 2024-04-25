251da5f9ef22373ca68e579232cb9458f7d57abb
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-25 23:19:53 +0700

    chore: handle token-interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TokenInterceptorProvider } from './config/interceptor.config';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
   ],
-  providers: []
+  providers: [
+    TokenInterceptorProvider
+  ]
 })
 export class CoreModule { }
--- src/app/main/_services/rating.service.ts
+++ src/app/main/_services/rating.service.ts
@@ -1,24 +1,15 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Rating } from '../_models';
-import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RatingService {
-  token = this.cookieService.get('accessToken');
-  private headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': `Token ${this.token}`,
-  })
-  constructor(
-    private http: HttpClient,
-    private cookieService: CookieService
-  ) { }
+  constructor(private http: HttpClient) { }
 
   get(movieId: number) {
-    return this.http.get<Rating[]>(`${environment.apiUrl}/ratings/by_movie/${movieId}`, { headers: this.headers });
+    return this.http.get<Rating[]>(`${environment.apiUrl}/ratings/by_movie/${movieId}`);
   }
 }
